1210473d82e9725153edb903af55cfadcb7b39ce
pblj <shkabarev.av@gmail.com>
2025-01-11 14:37:01 +0500

    dwq

--- Shooter/main.cpp
+++ Shooter/main.cpp
@@ -11,5 +11,12 @@ void main() {
 		key = _getch(); //ожидает ввод и возвращает аски код
 		cout << (int)key << "\t" << key << endl;
 
+		if (key == 'w')cout << "Вперед" << endl;
+		if (key == 's')cout << "Назад" << endl;
+		if (key == 'a')cout << "Влево" << endl;
+		if (key == 'd')cout << "Вправо" << endl;
+		if (key == ' ')cout << "Прыжок" << endl;
+		if (key == 13)cout << "Огонь" << endl;
+
 	} while (key != 27);
 }
