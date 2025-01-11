d62afb53ec21f1245f068d6f41cfaebcc9bcf836
pblj <shkabarev.av@gmail.com>
2025-01-11 14:05:58 +0500

    dwd

--- Shooter/main.cpp
+++ Shooter/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
 
 void main() {
 
 	setlocale(LC_ALL, "");
 	cout << "Shooter" << endl;
+	char key;
+	do {
+		key = _getch();
+		cout << (int)key << "\t" << key << endl;
 
+	} while (true);
 }
