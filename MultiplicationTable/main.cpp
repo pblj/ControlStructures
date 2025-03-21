191e573d9eed75e653703a3f8765112c2fa01fe8
shkabarev andrey <shkabarev.av@gmail.com>
2025-03-21 15:56:15 +0500

    1561

--- MultiplicationTable/main.cpp
+++ MultiplicationTable/main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 void main() {
 
+	setlocale(LC_ALL, "");
 
+	for (int i = 2; i < 10; i++) {
+
+		for (int l = 1; l <= 10; l++) {
+
+			cout << i << " * " << l << " = " << i * l << endl;
+
+		}
+
+	}
 
 }
